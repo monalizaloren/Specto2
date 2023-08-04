51bcb181bda821b24877f42b01cc9e9ebdedc36b
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-08-04 14:55:05 -0300

    Update Feed.js

--- screens/Feed.js
+++ screens/Feed.js
@@ -31,27 +31,7 @@ export default class Feed extends Component {
 
     render() {
         return (
-            <View style={styles.container}>
-                <SafeAreaView style={styles.droidSafeArea} />
-                <View style={styles.appTitle}>
-                    <View style={styles.appIcon}>
-                        <Image
-                            source={require("../assets/logo.png")}
-                            style={styles.iconImage}
-                        ></Image>
-                    </View>
-                    <View style={styles.appTitleTextContainer}>
-                        <Text style={styles.appTitleText}>Espectagrama</Text>
-                    </View>
-                </View>
-                <View style={styles.cardContainer}>
-                    <FlatList
-                        keyExtractor={this.keyExtractor}
-                        data={posts}
-                        renderItem={this.renderItem}
-                    />
-                </View>
-            </View>
+           //???
         );
     }
 }
