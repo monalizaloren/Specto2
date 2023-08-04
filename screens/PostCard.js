e90de1a627272ac8f252415b5f77a564dda005f4
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-08-04 14:54:33 -0300

    Update PostCard.js

--- screens/PostCard.js
+++ screens/PostCard.js
@@ -16,37 +16,7 @@ export default class PostCard extends Component {
 
     componentDidMount() { }
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <View style={styles.cardContainer}>
-                    <View style={styles.authorContainer}>
-                        <View style={styles.authorImageContainer}>
-                            <Image
-                                source={require("../assets/profile_img.png")}
-                                style={styles.profileImage}
-                            ></Image>
-                        </View>
-                        <View style={styles.authorNameContainer}>
-                            <Text style={styles.authorNameText}>{this.props.post.author}</Text>
-                        </View>
-                    </View>
-                    <Image source={require("../assets/post.jpeg")} style={styles.postImage} />
-                    <View style={styles.captionContainer}>
-                        <Text style={styles.captionText}>
-                            {this.props.post.caption}
-                        </Text>
-                    </View>
-                    <View style={styles.actionContainer}>
-                        <View style={styles.likeButton}>
-                            <Ionicons name={"heart"} size={RFValue(30)} color={"white"} />
-                            <Text style={styles.likeText}>12k</Text>
-                        </View>
-                    </View>
-                </View>
-            </View>
-        );
-    }
+//???
 }
 
 const styles = StyleSheet.create({
